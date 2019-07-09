97e9bb230c7aad300b014917b2d62dc84c753718
GadsDev <gusttavo212@gmail.com>
2019-07-08 22:29:45 -0300

    Get Total Gasto and quantGastos

--- deputados-api/app/Http/Controllers/Api/ReembolsoController.php
+++ deputados-api/app/Http/Controllers/Api/ReembolsoController.php
@@ -6,6 +6,7 @@ use App\Http\ApiError;
 use App\ReembolsoIndenisacao;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Deputado;
 
 class ReembolsoController extends Controller
 {    
@@ -24,5 +25,45 @@ class ReembolsoController extends Controller
         $data = $request->all();
         return ReembolsoIndenisacao::create($data);
     }
+
+    // Traz os gastos de todos os deputados com a API da Assembleia
+    public function getDeputadosAssembleiaApi()
+    {   
+        $deputadosId = $this->getAllDeputados();
+
+        for ($i=0; $i < 5; $i++) { 
+           $url = "http://dadosabertos.almg.gov.br/ws/prestacao_contas/".
+           "verbas_indenizatorias/deputados/15245/2017/5?formato=json"; 
+           
+           $gasto[$i] = json_decode(file_get_contents($url));
+           $detalhesGastos = count($gasto[0]->list[0]->listaDetalheVerba);
+           $gastoFormatado = array(               
+               'quantGastos' => $detalhesGastos,
+               'valorTotalMes' => $gasto[0]->list[0]->valor,
+           );
+        }
+      
+        /*
+        foreach ($deputadosId as $key => $value) {
+            $url = "http://dadosabertos.almg.gov.br/ws/prestacao_contas/".
+            "verbas_indenizatorias/deputados/".$value."/2017/1?formato=json";  
+            $gasto[$key] = json_decode(file_get_contents($url));
+        }*/
+
+       
+          
+        echo '<pre>' , var_dump($gastoFormatado) , '</pre>';
+        die();
+        return $diffDeputadosList;
+    }
+
+    public function getAllDeputados()
+    {   
+        $deputados = Deputado::where('id' ,'>' ,0)->pluck('id')->toArray();          
+        
+        
+        return $deputados;
+    }
+
 
 }
